1569c3abb882b3ff735ebf8c810ba5484aeac21a
Miftahul93 <miftahululum693@gmail.com>
2018-12-05 23:36:06 +0700

    Menambahkan logic button pindah di Mainactivity.java

--- app/src/main/java/com/example/denmas_mieftahh/myintentapp/MainActivity.java
+++ app/src/main/java/com/example/denmas_mieftahh/myintentapp/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.denmas_mieftahh.myintentapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnMoveActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnMoveActivity = (Button)findViewById(R.id.btn_move_activity);
+        btnMoveActivity.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_move_activity:
+                break;
+        }
     }
 }
